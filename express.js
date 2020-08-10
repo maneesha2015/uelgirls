a7595a03beff2e727c68186ce75e0cdbe220383e
Akinola Soyinka <44506742+AkinOlaSoyInka@users.noreply.github.com>
2020-08-10 15:48:22 +0100

    Update express.js

--- express.js
+++ express.js
@@ -1,9 +1,11 @@
 express = require('express')
+bodyParser = require("body-parser");
 app = express()
 app.listen(3000, function(){
 
     console.log('webserver is running on port 3000S')
 }) 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/list', function(req,res){
 
